eea43e3615a4540ccda369a48ce972f5b9b8faf9
BahaaEbraheem <86316666+BahaaEbraheem@users.noreply.github.com>
2022-04-23 23:52:24 +0300

    Commit

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-
+ BahaaForm  = new FormGroup({
+    Name: new FormControl(''),
+    Age: new FormControl(''),
+    Address: new FormControl(''),
+    Job: new FormControl(''),
+  })
   constructor() { }
-
+  Submit(){
+    console.log(this.BahaaForm.value);
+  }
   ngOnInit(): void {
   }
 
